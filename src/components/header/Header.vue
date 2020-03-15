577e96721963569365769474018e33e4d6597659
adrianorod <adrianoazevedo.r@gmail.com>
2020-03-15 19:44:43 -0300

    feat: cria base do component Header

---
+++ src/components/header/Header.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-app-bar app class="header" height="96">
+
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+
+  data: () => ({
+  }),
+};
+</script>
+
+<style lang="scss" scoped>
+.header {
+  box-shadow: 0px -5px 18px -1px rgba(0, 0, 0, 0.2);
+}
+</style>
