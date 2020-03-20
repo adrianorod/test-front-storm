b0343954653f5ed50d1bdcee0c764257c3440273
adriano.rodrigues <adriano.rodrigues@wooza.com.br>
2020-03-20 11:25:42 -0300

    feat: cria UserList component

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,24 @@
 <template>
   <v-app class="app">
     <Header />
-
     <v-content>
+      <v-container fluid class="px-9 py-6">
+        <UserList />
+      </v-container>
     </v-content>
   </v-app>
 </template>
 
 <script>
 import Header from './components/header/Header.vue';
+import UserList from './components/user-list/UserList.vue';
 
 export default {
   name: 'App',
 
   components: {
     Header,
+    UserList,
   },
 
   data: () => ({
---
+++ src/components/user-list/UserList.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-data-table
+    :headers="headers"
+    :items="users"
+    item-key="id"
+    show-select
+  />
+</template>
+
+<script>
+export default {
+  name: 'UserList',
+
+  data: () => ({
+    headers: [
+      { text: 'USUÁRIO', value: 'user' },
+      { text: 'EMAIL', value: 'email' },
+      { text: 'DATA DE INCLUSÃO', value: 'inclusionDate' },
+      { text: 'DATA DE ALTERAÇÃO', value: 'changeDate' },
+      { text: 'REGRAS', value: 'rules' },
+      { text: 'STATUS', value: 'status' },
+      { text: 'AÇÕES', value: 'acoes' },
+    ],
+  }),
+
+  mounted() {
+    this.$store.dispatch('users/loadUsers');
+  },
+
+  computed: {
+    users() {
+      return this.$store.state.users.users;
+    },
+  },
+};
+</script>
