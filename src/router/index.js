e17194d2f98896570a5f35501ade43a8313f9400
adrianorod <adrianoazevedo.r@gmail.com>
2020-03-22 19:52:01 -0300

    chore: adiciona vue-router como dependencia

---
+++ src/router/index.js
@@ -0,0 +1,19 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import User from '../views/User.vue';
+
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: '/',
+    name: 'User',
+    component: User,
+  },
+];
+
+const router = new VueRouter({
+  routes,
+});
+
+export default router;
